a3e08f6b1fe43a0587d54e320e814f202a3dbbba
Gexedd <eeviac.cl@gmail.com>
2023-09-28 09:42:08 -0300

    Actualizacion del codigo

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    super.key,
-  });
+class TaskTile extends StatefulWidget {
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('Esta es una tarea'),
-      trailing: Checkbox(value: false, onChanged: null),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+
+final bool chekBoxState;
+TaskCheckbox(this.chekBoxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: chekBoxState,
+        onChanged: (newValue){
+/*          setState(() {
+            isChecked = newValue!;
+          });*/
+
+        },
     );
   }
 }
